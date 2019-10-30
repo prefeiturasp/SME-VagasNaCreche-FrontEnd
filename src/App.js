7242385f8a5796aec212936d1c142f45e7da1a46
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-10-30 11:23:31 -0300

    Criando Hello World - Vaga na Creche

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <section role="main">
+      <section className="container">
+        <h1>Bem-Vindo ao Vaga na Creche</h1>
+
+      </section>
+
+    </section>
+  );
+}
+
+export default App;
