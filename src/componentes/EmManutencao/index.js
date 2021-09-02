b9e46cdd7aaf5e71e77c61c23f3a19b43bdfe001
ollyvergithub <ollyverottoboni@gmail.com>
2021-09-02 17:14:17 -0300

    feat(48461): Tranforma página em manutenção em Class Component

--- src/componentes/EmManutencao/index.js
+++ src/componentes/EmManutencao/index.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, {Fragment} from "react";
 import BarraSuperior from "../../utils/BarraSuperior";
 import PubSub from "pubsub-js";
 
-export const EmManutencao = () =>{
-    PubSub.publish("mostraLinkHome", true);
-    return (
-        <>
-            <BarraSuperior texto="Em manutenção" filtro={false}/>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-lg-6 mt-5">
-                        <p className="fonte-16">
-                            Esta página encontra-se em manutenção
-                        </p>
+class EmManutencao extends React.Component{
+    constructor() {
+        super();
+        PubSub.publish("mostraLinkHome", true);
+    }
+
+    render() {
+        return(
+            <Fragment>
+                <BarraSuperior texto="Em manutenção" filtro={false}/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-lg-6 mt-5">
+                            <p className="fonte-16">Esta página encontra-se em manutenção</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </Fragment>
+        )
+    }
 }
+
+export default EmManutencao
